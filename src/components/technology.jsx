8628ec661ce744ac6954bfff2219e5f3d5482a3e
Muhammad Ahmed <ahmedmughal3182@gmail.com>
2024-04-02 17:48:49 +0500

    tech

--- src/components/technology.jsx
+++ src/components/technology.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
-const technology = () => {
-  return <div></div>;
+const Technology = () => {
+  return (
+    <div>
+      <div className="bg-darkPrimary">
+        <div className="flex flex-wrap p-4 py-10 items-center gap-16 justify-center">
+          <img
+            className="w-20 "
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+            alt="react"
+          />
+          <img
+            className="w-20 "
+            src="https://once-and-only.com/wp-content/uploads/2020/09/nodejs.png"
+            alt="react"
+          />
+          <img
+            className="w-20 "
+            src="https://cdn3d.iconscout.com/3d/free/thumb/free-javascript-9294848-7577991.png?f=webp"
+            alt="react"
+          />
+          <img
+            className="w-20 "
+            src="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png"
+            alt="react"
+          />
+          <img
+            className="w-20 "
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png"
+            alt="react"
+          />
+          <img
+            className="w-20 "
+            src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png?f=webp"
+            alt="react"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default technology;
+export default Technology;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/HeroSection";
 import HireMe from "../components/HireMe";
 import Projects from "../components/Projects";
 import { useEffect } from "react";
+import Technology from "../components/technology";
 
 const Home = () => {
   useEffect(() => {
@@ -12,6 +13,7 @@ const Home = () => {
     <>
       <div className="overflow-x-hidden">
         <HeroSection />
+        <Technology />
         <HireMe />
         <Projects />
       </div>
