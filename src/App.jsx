b13328927e3a13a3d7725b816aa693fccc5819ef
Muhammad Ahmed <ahmedmughal3182@gmail.com>
2024-04-03 12:22:39 +0500

    res menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,29 @@ import Project from "./pages/Project";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [sticky, setSticky] = useState(false);
+  const [Showmenu, setShowmenu] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setShowmenu(false);
+    const handleScroll = () => {
+      setSticky(window.scrollY > 40);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <Router>
-        <NavBar />
-
+        <NavBar sticky={sticky} Showmenu={Showmenu} setShowmenu={setShowmenu} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Project />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import logo from "../assets/img/logo_arabics.svg";
+import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ Showmenu, setShowmenu }) => {
+  const handelToggle = () => {
+    setShowmenu(true);
+  };
   const navigate = useNavigate();
 
   return (
@@ -15,38 +19,51 @@ const NavBar = () => {
           }}>
           <img className="w-14 p-1 logoImg" src={logo} alt="Ahmed.Dev" />
         </span>
-        <div className="flex gap-4 uppercase justify-evenly items-center sm:hidden md:hidden">
-          <p
-            className="cursor-pointer text-sm space2 link p-1 "
-            onClick={() => {
-              // location.href = "/";
-              navigate("/");
-            }}>
-            Home
-          </p>
-          {/* <p
-            className="cursor-pointer text-sm space2 link p-1"
-            onClick={scrollToAbout}>
-            About
-          </p> */}
-          <p
-            className="cursor-pointer text-sm space2 link p-1"
-            onClick={() => {
-              navigate("/projects");
-              // location.href = "/projects";
-            }}>
-            Projects
-          </p>
-          <p
-            className="cursor-pointer text-sm space2 link p-1"
-            onClick={() => {
-              navigate("/contact");
-              // location.href = "/projects";
-            }}>
-            Contact me
-          </p>
-        </div>
+
+        <span className="lg:hidden  sm:block s-md:block md:block">
+          <i
+            className="ri-menu-4-fill text-[#ffffff] text-2xl cursor-pointer p-1 transform ease-in-out duration-300"
+            onClick={handelToggle}></i>
+        </span>
       </div>
+
+      {Showmenu && (
+        <div className="flex  fixed top-0 flex-col bg-primary  resNav">
+          <span>
+            <i
+              class="ri-close-fill text-[#ffffff] text-4xl cursor-pointer p-5 float-end"
+              onClick={() => {
+                setShowmenu(false);
+              }}></i>
+          </span>
+          <ul className=" flex flex-col text-3xl resNav gap-4 uppercase items-center justify-center  font-semibold py-1 px-2 cursor-pointer z-50 transform duration-300 ease-linear  ">
+            <NavLink
+              to="/"
+              activeClassName="font-bold"
+              onClick={() => {
+                setShowmenu(false);
+              }}>
+              <li className=" hover:font-bold link">Home</li>
+            </NavLink>
+            <NavLink
+              to="/projects"
+              activeClassName="font-bold "
+              onClick={() => {
+                setShowmenu(false);
+              }}>
+              <li className=" hover:font-bold link">Projects</li>
+            </NavLink>
+            <NavLink
+              to="/contact"
+              activeClassName="font-bold"
+              onClick={() => {
+                setShowmenu(false);
+              }}>
+              <li className=" hover:font-bold link">Contact</li>
+            </NavLink>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
